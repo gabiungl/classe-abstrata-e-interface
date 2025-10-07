fca01328134001862f9610f4b0ab2e1789e7dce9
gabiungl <gabiungarette@gmail.com>
2025-10-07 15:44:57 -0300

    teste

---
+++ app.py
@@ -0,0 +1,8 @@
+from funcoes import *
+
+leao = Leao("Simba", 5)
+pinguim = Pinguim("Pingo", 3)
+aguia = Aguia("Águia Dourada", 7)
+
+animais = [leao, pinguim, aguia]
+interagir_no_zoologico(animais)
